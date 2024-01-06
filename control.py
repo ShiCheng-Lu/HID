57790901589ea858b059ad988c1ed5581d988e6b
ShiCheng Lu <ShiChengLu0708@gmail.com>
2024-01-06 01:54:29 -0500

    default to not update mouse report value when joystick is not interacted with

--- control.py
+++ control.py
@@ -20,20 +20,33 @@ class KeyboardControl:
     def __init__(self):
         self.device = usb.core.find(idVendor=0x16c0, idProduct=0x27da,
                                     custom_match=MatchHIDProtocol(MatchHIDProtocol.KEYBOARD))
+        self.device.set_configuration()
+        self.interface = self.device[0][(0, 0)].bInterfaceNumber
+
+    def report(self, modifier, keys):
+        pass
 
 
 class MouseControl:
     def __init__(self):
         self.device = usb.core.find(idVendor=0x16c0, idProduct=0x27da,
                                     custom_match=MatchHIDProtocol(MatchHIDProtocol.MOUSE))
+        self.device.set_configuration()
+        self.interface = self.device[0][(0, 0)].bInterfaceNumber
 
-    def move(self, button, x, y):
+    def report(self, button, x, y):
         self.device.ctrl_transfer(
             0x21,  # REQUEST_TYPE_CLASS | RECIPIENT_INTERFACE | ENDPOINT_OUT
             9,     # SET_REPORT
             0x200,  # "Vendor" Descriptor Type + 0 Descriptor Index
-            1,     # USB interface # 0
+            self.interface,     # USB interface
             # the HID payload as a byte array -- e.g. from struct.pack()
             struct.pack("<Bbb", button, x, y),
         )
+    
+    def move(self, x, y):
+        pass
 
+
+keyboard = KeyboardControl()
+mouse = MouseControl()
--- peripheral/src/main.cpp
+++ peripheral/src/main.cpp
@@ -2,9 +2,10 @@
 
 #define JOYSTICK_VRX A0
 #define JOYSTICK_VRY A1
-#define JOYSTICK_BTN 2
-#define ENABLE_OUTPUT 3
-#define LED_PIN 4
+#define JOYSTICK_BTN A2
+#define ENABLE_OUTPUT 11
+
+#define JOYSTICK_DEADZONE 5
 
 typedef struct MouseReport_t {
     uint8_t buttons;
@@ -16,55 +17,51 @@ void setup() {
     // init joystick
     pinMode(JOYSTICK_VRX, INPUT);
     pinMode(JOYSTICK_VRY, INPUT);
-    pinMode(JOYSTICK_BTN, INPUT);
-    // button to enable/disable joystick reports
-    pinMode(ENABLE_OUTPUT, INPUT);
+    pinMode(JOYSTICK_BTN, INPUT_PULLUP);
     Serial.begin(9600);
 
-    pinMode(LED_PIN, OUTPUT);
+    pinMode(LED_BUILTIN, OUTPUT);
 }
 
+void filterDeadzone(MouseReport_t* mouse) {
+    if (mouse->x > -JOYSTICK_DEADZONE && mouse->x < JOYSTICK_DEADZONE) {
+        mouse->x = 0;
+    }
+    if (mouse->y > -JOYSTICK_DEADZONE && mouse->y < JOYSTICK_DEADZONE) {
+        mouse->y = 0;
+    }
+}
+
+static bool last_report_had_input = false;
 void updateJoystick() {
-    MouseReport_t report = {
-        .buttons = (uint8_t)digitalRead(JOYSTICK_BTN),
-        .x = (int8_t)((511 - analogRead(JOYSTICK_VRX)) / 4),
-        .y = (int8_t)((511 - analogRead(JOYSTICK_VRY)) / 4),
+    MouseReport_t mouse = {
+        .buttons = !(uint8_t)digitalRead(JOYSTICK_BTN),
+        .x = (int8_t)((analogRead(JOYSTICK_VRY) - 512) / 4),
+        .y = (int8_t)((511 - analogRead(JOYSTICK_VRX)) / 4),
     };
+    filterDeadzone(&mouse);
+
+    if (mouse.buttons || mouse.x || mouse.y) {
+        last_report_had_input = true;
+        digitalWrite(LED_BUILTIN, HIGH);
+    } else {
+        if (last_report_had_input) {
+            // if the last state had an input but current state does not,
+            // send a zero rert to poreset the values
+            last_report_had_input = false;
+            mouse.x = 0;
+            mouse.y = 0;
+        } else {
+            // do not send a report if there is no input
+            digitalWrite(LED_BUILTIN, LOW);
+            return;
+        }
+    }
 
     // send 0b100<size:5> to begin report transmission
     // this is picked up by usb_interface to receive a mouse report
-    Serial.write(0x80 + sizeof(report));
-    Serial.write((uint8_t*)&report, sizeof(report));
-}
-
-static int enabled = false;
-static int prev_state = LOW;
-/**
- * @brief enable/disable reporting joystick mouse based on button state
- * Joystick will override echo behaviour of the HID device,
- * when joystick is enabled, the device cannot be controlled from host, instead,
- * its controlled by the joystick.
- */
-void updateEnable(void) {
-    int state = digitalRead(ENABLE_OUTPUT);
-    if (state != prev_state) {
-        prev_state = state;
-        enabled = !enabled;
-    }
-}
-
-static int state = HIGH;
-void updateLED(void) {
-    int data = Serial.read();
-    if (data < 0) {
-        return;
-    }
-    // clear buffer
-    while (Serial.read() >= 0) {
-    }
-
-    digitalWrite(LED_PIN, state);
-    state = !state;
+    Serial.write(0x80 + sizeof(mouse));
+    Serial.write((uint8_t*)&mouse, sizeof(mouse));
 }
 
 int main(void) {
@@ -72,12 +69,8 @@ int main(void) {
     setup();
 
     while (true) {
-        updateLED();
-        updateEnable();
-        if (enabled) {
-            updateJoystick();
-            serialEventRun();
-        }
+        updateJoystick();
+        serialEventRun();
     }
 
     return 0;
